22d4202c6b92085c2dfc92bac4a375bbdabce5b1
Rahul Sinha <48615211+boy-who-codes@users.noreply.github.com>
2020-06-21 19:07:46 +0530

    Update webrtc.js

--- webrtc.js
+++ webrtc.js
@@ -14,3 +14,161 @@ function start() {
     },
     audio: false,
   };
+// set up local video stream
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(stream => {
+        localStream = stream;
+        document.getElementById('localVideo').srcObject = stream;
+      }).catch(errorHandler)
+ 
+      // set up websocket and message all existing clients
+      .then(() => {
+        serverConnection = new WebSocket('wss://' + window.location.hostname + ':' + WS_PORT);
+        serverConnection.onmessage = gotMessageFromServer;
+        serverConnection.onopen = event => {
+          serverConnection.send(JSON.stringify({ 'displayName': localDisplayName, 'uuid': localUuid, 'dest': 'all' }));
+        }
+      }).catch(errorHandler);
+ 
+  } else {
+    alert('Your browser does not support getUserMedia API');
+  }
+}
+function gotMessageFromServer(message) {
+  var signal = JSON.parse(message.data);
+  var peerUuid = signal.uuid;
+ 
+  // Ignore messages that are not for us or from ourselves
+  if (peerUuid == localUuid || (signal.dest != localUuid && signal.dest != 'all')) return;
+ 
+  if (signal.displayName && signal.dest == 'all') {
+    // set up peer connection object for a newcomer peer
+    setUpPeer(peerUuid, signal.displayName);
+    serverConnection.send(JSON.stringify({ 'displayName': localDisplayName, 'uuid': localUuid, 'dest': peerUuid }));
+ 
+  } else if (signal.displayName && signal.dest == localUuid) {
+    // initiate call if we are the newcomer peer
+    setUpPeer(peerUuid, signal.displayName, true);
+ 
+  } else if (signal.sdp) {
+    peerConnections[peerUuid].pc.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function () {
+      // Only create answers in response to offers
+      if (signal.sdp.type == 'offer') {
+        peerConnections[peerUuid].pc.createAnswer().then(description => createdDescription(description, peerUuid)).catch(errorHandler);
+      }
+    }).catch(errorHandler);
+ 
+  } else if (signal.ice) {
+    peerConnections[peerUuid].pc.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
+  }
+}
+function setUpPeer(peerUuid, displayName, initCall = false) {
+  peerConnections[peerUuid] = { 'displayName': displayName, 'pc': new RTCPeerConnection(peerConnectionConfig) };
+  peerConnections[peerUuid].pc.onicecandidate = event => gotIceCandidate(event, peerUuid);
+  peerConnections[peerUuid].pc.ontrack = event => gotRemoteStream(event, peerUuid);
+  peerConnections[peerUuid].pc.oniceconnectionstatechange = event => checkPeerDisconnect(event, peerUuid);
+  peerConnections[peerUuid].pc.addStream(localStream);
+ 
+  if (initCall) {
+    peerConnections[peerUuid].pc.createOffer().then(description => createdDescription(description, peerUuid)).catch(errorHandler);
+  }
+}
+function gotRemoteStream(event, peerUuid) {
+  console.log(`got remote stream, peer ${peerUuid}`);
+  //assign stream to new HTML video element
+  var vidElement = document.createElement('video');
+  vidElement.setAttribute('autoplay', '');
+  vidElement.setAttribute('muted', '');
+  vidElement.srcObject = event.streams[0];
+ 
+  var vidContainer = document.createElement('div');
+  vidContainer.setAttribute('id', 'remoteVideo_' + peerUuid);
+  vidContainer.setAttribute('class', 'videoContainer');
+  vidContainer.appendChild(vidElement);
+  vidContainer.appendChild(makeLabel(peerConnections[peerUuid].displayName));
+ 
+  document.getElementById('videos').appendChild(vidContainer);
+ 
+  updateLayout();
+}
+ 
+ 
+function updateLayout() {
+  // update CSS grid based on number of diplayed videos
+  var rowHeight = '98vh';
+  var colWidth = '98vw';
+ 
+  var numVideos = Object.keys(peerConnections).length + 1; // add one to include local video
+ 
+  if (numVideos > 1 && numVideos <= 4) { // 2x2 grid
+    rowHeight = '48vh';
+    colWidth = '48vw';
+  } else if (numVideos > 4) { // 3x3 grid
+    rowHeight = '32vh';
+    colWidth = '32vw';
+  }
+ 
+  document.documentElement.style.setProperty(`--rowHeight`, rowHeight);
+  document.documentElement.style.setProperty(`--colWidth`, colWidth);
+}
+ 
+function makeLabel(label) {
+  var vidLabel = document.createElement('div');
+  vidLabel.appendChild(document.createTextNode(label));
+  vidLabel.setAttribute('class', 'videoLabel');
+  return vidLabel;
+}
+
+function checkPeerDisconnect(event, peerUuid) {
+  var state = peerConnections[peerUuid].pc.iceConnectionState;
+  console.log(`connection with peer ${peerUuid} ${state}`);
+  if (state === "failed" || state === "closed" || state === "disconnected") {
+    delete peerConnections[peerUuid];
+    document.getElementById('videos').removeChild(document.getElementById('remoteVideo_' + peerUuid));
+    updateLayout();
+  }
+}
+
+// Create a server for the client html page
+const handleRequest = function (request, response) {
+  // Render the single client html file for any request the HTTP server receives
+  console.log('request received: ' + request.url);
+ 
+ if (request.url === '/webrtc.js') {
+    response.writeHead(200, { 'Content-Type': 'application/javascript' });
+    response.end(fs.readFileSync('client/webrtc.js'));
+  } else if (request.url === '/style.css') {
+    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.end(fs.readFileSync('client/style.css'));
+  } else {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(fs.readFileSync('client/index.html'));
+  }
+};
+ 
+const httpsServer = https.createServer(serverConfig, handleRequest);
+httpsServer.listen(HTTPS_PORT);
+ 
+// ----------------------------------------------------------------------------------------
+ 
+// Create a server for handling websocket calls
+const wss = new WebSocketServer({ server: httpsServer });
+ 
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    // Broadcast any received message to all clients
+    console.log('received: %s', message);
+    wss.broadcast(message);
+  });
+ 
+  ws.on('error', () => ws.terminate());
+});
+ 
+wss.broadcast = function (data) {
+  this.clients.forEach(function (client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
